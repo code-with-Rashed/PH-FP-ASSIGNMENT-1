272bae65acce23a100c1b7d951bc09746d48c981
Rashed <rashedalamj@gmail.com>
2026-09-22 12:48:42 +0600

    solve question 2

--- answer.js
+++ answer.js
@@ -7,3 +7,24 @@ function describeValue(value) {
     return `${$valueType} | falsy`;
   }
 }
+
+// question 2 solution
+function getDayType(day) {
+  const dayName = day.toLowerCase();
+
+  switch (dayName) {
+    case "friday":
+    case "saturday":
+      return "Weekend";
+
+    case "sunday":
+    case "monday":
+    case "tuesday":
+    case "wednesday":
+    case "thursday":
+      return "Working Day";
+
+    default:
+      return "Invalid Day";
+  }
+}
